68ca8c7ca43bde0348d4ffbeed51b619d279cb10
Nadia <nadiagueu@gmail.com>
2022-12-30 03:45:47 +0000

    week2 day3 dailychallenge

---
+++ week2/day3/Dailychallenge/script.js
@@ -0,0 +1,11 @@
+
+let numbermax = 6;
+
+for(let i =1; i<numbermax; i++){
+let numberstars =i+1;
+let rangstars ="";
+   for(let j =0; j<numberstars; j++){
+    rangstars = rangstars + " * "
+   }
+   console.log(rangstars); 
+}
